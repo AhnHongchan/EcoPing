9524ea20991f691d18fbd0437d77a9a3c72d8217
강성구 <nastar9@naver.com>
2024-09-06 15:55:53 +0900

    Refactor: [BE] Api호출 유틸 생성

--- backend/src/main/java/com/f1veguys/sel/global/util/ApiUtil.java
+++ backend/src/main/java/com/f1veguys/sel/global/util/ApiUtil.java
@@ -1,2 +1,28 @@
-package com.f1veguys.sel.global.util;public class ApiUtil {
+package com.f1veguys.sel.global.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class ApiUtil {
+
+
+    private final WebClient webClient;
+
+    public Mono<Map<String, Object>> callFinApi(String url, HttpMethod method, Object body) {
+        return webClient.method(method)
+                .uri(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(body)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {});
+    }
 }
