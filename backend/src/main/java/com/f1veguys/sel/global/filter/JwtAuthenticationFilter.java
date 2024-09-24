1c1143a36532ee265435d04fe72e83f0d3b6dbf8
강성구 <nastar9@naver.com>
2024-09-24 16:54:37 +0900

    Fix:[BE] Security 수정

--- backend/src/main/java/com/f1veguys/sel/global/filter/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/f1veguys/sel/global/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,7 @@
 package com.f1veguys.sel.global.filter;
 
+import com.f1veguys.sel.domain.customuser.CustomUserDetails;
+import com.f1veguys.sel.domain.customuser.service.CustomUserDetailsService;
 import com.f1veguys.sel.global.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -24,7 +26,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 
     @Autowired
-    private UserDetailsService userDetailsService;
+    private CustomUserDetailsService userDetailsService;
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        // 로그인과 회원가입을 제외한 모든 경로에 대해 필터 적용
+        return path.startsWith("/api/users")
+                ;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -32,20 +42,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         // 요청 헤더에서 "Authorization" 헤더를 가져옴
         final String authorizationHeader = request.getHeader("Authorization");
 
-        String username = null;
+        String userEmail = null;
         String jwt = null;
 
         // Authorization 헤더가 있고 "Bearer "로 시작하는 경우 JWT 토큰을 추출
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7); // "Bearer " 이후의 JWT 토큰 부분만 추출
-            username = jwtUtil.getUsernameFromToken(jwt); // JWT에서 사용자 이름 추출
+            userEmail = jwtUtil.getUsernameFromToken(jwt); // JWT에서 사용자 이메일 추출
         }
 
         // 사용자가 인증되지 않았고 유효한 사용자 이름이 있는 경우
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             // 사용자 이름으로 사용자 정보 로드
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
-
+            CustomUserDetails userDetails = (CustomUserDetails) this.userDetailsService.loadUserByUsername(userEmail);
             // JWT 토큰이 유효한 경우
             if (jwtUtil.validateToken(jwt)) {
                 // 사용자 인증을 위한 인증 토큰 생성
@@ -56,6 +65,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 // 보안 컨텍스트에 인증 정보 설정
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+                request.setAttribute("email", userDetails.getUsername());
+                request.setAttribute("id", userDetails.getId());
             }
         }
         // 다음 필터로 요청과 응답을 전달
