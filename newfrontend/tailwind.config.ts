edb2caff52585b0868a2b99c6c0a8f9e24baefc1
박상용 <psyok7@naver.com>
2024-09-24 13:31:31 +0900

    Design: [FE] 로그인 페이지 CSS 추가

--- newfrontend/tailwind.config.ts
+++ newfrontend/tailwind.config.ts
@@ -13,7 +13,9 @@ const config: Config = {
         lightWalnutBrown: "#A68A6D", // 나무 껍질 색
         mainGreen: "#bcebc4", // 연한 이끼색
         coralRed: "#e57373",
-        mainDarkGreen: "#3c4a3e"
+        mainDarkGreen: "#3c4a3e",
+        loginLightGreen: "rgb(218,229,221)",
+        loginDarkGreen: "rgb(64,116,77)"
       },
       boxShadow: {
         'custom-lg': '0px 4px 10px 2px rgba(0, 0, 0, 0.1)',
