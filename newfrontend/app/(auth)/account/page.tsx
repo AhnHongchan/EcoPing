821ad91df4301b9f289f04e654333f794188197c
박상용 <psyok7@naver.com>
2024-09-27 15:13:58 +0900

    Feat: [FE] 나무페이지 기능 추가

--- newfrontend/app/(auth)/account/page.tsx
+++ newfrontend/app/(auth)/account/page.tsx
@@ -127,7 +127,7 @@ const CreateAccount = () => {
         email,
         password,
         phoneNumber,
-      });
+      }); 
   
       if (response.status === 200) {
         alert("회원 가입이 성공적으로 완료되었습니다!");
