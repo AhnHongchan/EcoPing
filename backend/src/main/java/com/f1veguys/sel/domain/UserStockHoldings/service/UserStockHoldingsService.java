b2024555aa8467e8d97a0213063dbcc98349ac70
박진형 <dyunames21@gmail.com>
2024-10-04 11:23:28 +0900

    fix : 유저 보유 주식 부분 에러 해결

--- backend/src/main/java/com/f1veguys/sel/domain/UserStockHoldings/service/UserStockHoldingsService.java
+++ backend/src/main/java/com/f1veguys/sel/domain/UserStockHoldings/service/UserStockHoldingsService.java
@@ -1,14 +1,15 @@
 package com.f1veguys.sel.domain.UserStockHoldings.service;
 
 import com.f1veguys.sel.domain.UserStockHoldings.domain.UserStockHoldings;
+import com.f1veguys.sel.domain.UserStockHoldings.dto.UserStockHoldingsDto;
 import com.f1veguys.sel.domain.company.domain.Company;
 import com.f1veguys.sel.domain.user.domain.User;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface UserStockHoldingsService {
     UserStockHoldings updateHoldings(User user, Company company, int quantity, boolean isBuy, double purchasePrice);
     int getHoldings(User user, Company company);
     double getAveragePurchasePrice(User user, Company company);
-    Optional<UserStockHoldings> getUserStockHoldings(Company company, User user);
+    List<UserStockHoldingsDto> getUserStockHoldingsList(User user);
 }
