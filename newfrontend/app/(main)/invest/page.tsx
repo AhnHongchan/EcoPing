7789f68b9f77f477a7d455a40225fb7a43f938be
AhnHongchan <hha0408@gmail.com>
2024-10-06 14:46:12 +0900

    feat: websocket 연결 성공

--- newfrontend/app/(main)/invest/page.tsx
+++ newfrontend/app/(main)/invest/page.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { useState, useEffect } from "react";
+import instance from "@/lib/axios";
+
 import { BiSolidHeart } from "react-icons/bi";
 
 interface StockItem {
@@ -13,37 +15,68 @@ interface StockItem {
 }
 
 const Investment = (): JSX.Element => {
-  const [stockList, setStockList] = useState<StockItem[]>([]); // 초기값을 빈 배열로 설정
+  const [stockList, setStockList] = useState<StockItem[]>([]);
+  const [nameList, setNameList] = useState<{ [key: string]: string }>({});
+  const [holdList, setHoldList] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
     // 웹소켓 연결 설정
     const socket = new WebSocket("ws://localhost:8080/websocket/stock");
 
-    // 웹소켓이 열렸을 때
-    socket.onopen = () => {
-      console.log("WebSocket 연결 성공");
+    const fetchWholeStockData = async () => {
+      const nameResponse = await instance.get("/stock/list");
+      const holdResponse = await instance.get("holdings/list");
+
+      const nameData = nameResponse.data.data;
+      const holdData = holdResponse;
+      const companyDict = nameData.reduce((acc, item) => {
+        // 각 객체에서 companyNumber와 companyName을 추출하고 딕셔너리에 추가
+        acc[item.companyNumber] = item.companyName;
+        return acc;
+      }, {}); // 초기값을 빈 객체로 설정
+
+      setNameList(companyDict);
+      setHoldList(holdData);
+
+      // 웹소켓이 열렸을 때
+      socket.onopen = () => {
+        console.log("WebSocket 연결 성공");
+      };
+
+      // 웹소켓을 통해 메시지를 받을 때
+      socket.onmessage = (event) => {
+        try {
+          const data = JSON.parse(event.data);
+
+          if (data?.header?.tr_id === "PINGPONG") {
+            return;
+          }
+
+          // 데이터가 배열인지 확인하고 상태 업데이트
+          if (Array.isArray(data)) {
+            setStockList(data);
+          } else if (data !== null && typeof data === "object") {
+            // 데이터가 객체인 경우 배열로 감싸기
+            setStockList([data]);
+          } else {
+            setStockList((prevList) => [...prevList]); // 기존 상태 유지
+          }
+        } catch (error) {}
+      };
+
+      // 웹소켓 연결이 닫힐 때
+      socket.onclose = () => {
+        console.log("WebSocket 연결 종료");
+      };
+
+      // 컴포넌트 언마운트 시 웹소켓 연결 닫기
+      return () => {
+        socket.close();
+      };
     };
 
-    // 웹소켓을 통해 메시지를 받을 때
-    socket.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log("수신한 데이터:", data); // 데이터 구조를 확인하기 위해 콘솔에 출력
-
-      // 데이터가 배열인지 확인하고 상태 업데이트
-
-      setStockList(data);
-    };
-
-    // 웹소켓 연결이 닫힐 때
-    socket.onclose = () => {
-      console.log("WebSocket 연결 종료");
-    };
-
-    // 컴포넌트 언마운트 시 웹소켓 연결 닫기
-    return () => {
-      socket.close();
-    };
+    fetchWholeStockData();
   }, []);
 
   // 주식 상세 페이지로 이동하는 함수
@@ -57,12 +90,12 @@ const Investment = (): JSX.Element => {
         <p className="text-center text-2xl">모의 투자</p>
       </div>
       <br />
-      <div>
+      {/* <div>
         <p className="font-bold text-xl pl-2">보유 종목</p>
       </div>
       <div className="items-center">
         <div className="overflow-y-scroll scrollbar-hide my-2 pb-2 max-h-[320px]">
-          {stockList.map((stock) => (
+          {holdList.map((stock) => (
             <div
               key={stock.companyNumber}
               onClick={() => handleClick(stock)}
@@ -70,12 +103,12 @@ const Investment = (): JSX.Element => {
             >
               <div className="shrink-0 col-span-7">
                 <p className="text-black font-bold text-base leading-normal">
-                  {stock.stockName}
+                  {stock.companyName}
                 </p>
               </div>
               <div className="flex flex-col justify-center col-span-5">
                 <p className="text-black text-base font-bold leading-normal line-clamp-1">
-                  현재가: {parseInt(stock.currentPrice).toLocaleString()}
+                  현재가: {parseInt(stock.hold).toLocaleString()}
                 </p>
                 <p className="text-[#93a7c8] text-sm font-medium leading-normal line-clamp-2">
                   내 수익률: 5%
@@ -84,7 +117,7 @@ const Investment = (): JSX.Element => {
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
       <br />
       <div>
         <p className="font-bold text-xl pl-2">전체 종목</p>
@@ -98,7 +131,7 @@ const Investment = (): JSX.Element => {
           >
             <div className="col-span-7 flex items-center min-h-12">
               <p className="text-sm text-center font-bold ml-2">
-                {stock.companyNumber}
+                {nameList[stock.companyNumber] || stock.companyNumber}
               </p>
             </div>
             <div className="col-span-5">
