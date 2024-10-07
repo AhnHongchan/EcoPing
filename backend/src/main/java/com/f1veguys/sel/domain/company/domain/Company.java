61d6703d0d0e7132a124abd9969518cecd66ffc2
박진형 <dyunames21@gmail.com>
2024-10-07 17:42:13 +0900

    chore 복구

--- backend/src/main/java/com/f1veguys/sel/domain/company/domain/Company.java
+++ backend/src/main/java/com/f1veguys/sel/domain/company/domain/Company.java
@@ -18,5 +18,5 @@ public class Company {
     private String companyName;
     private String companyNumber;
     private double ecoScore;
-    private int ranking; // 순위 필드 추가
+    private int ranking; // 순위 필드 추가 함
 }
