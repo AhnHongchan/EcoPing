909eed8272a15b8803152c15365874b5d26a41f0
AhnHongchan <hha0408@gmail.com>
2024-10-08 16:57:03 +0900

    그래프 완료

--- newfrontend/app/(main)/invest/page.tsx
+++ newfrontend/app/(main)/invest/page.tsx
@@ -66,7 +66,7 @@ const Investment = (): JSX.Element => {
 
   // WebSocket 연결 및 데이터 처리 함수
   const initializeWebSocket = () => {
-    const socket = new WebSocket("ws://j11a304.p.ssafy.io/websocket/stock");
+    const socket = new WebSocket("wss://j11a304.p.ssafy.io/websocket/stock");
 
     socket.onopen = () => {
       console.log("WebSocket 연결 성공");
