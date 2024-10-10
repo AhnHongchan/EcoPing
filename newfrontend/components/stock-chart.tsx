0d4d32a3702d9d7ed516df7d41942d7f23332fe0
AhnHongchan <hha0408@gmail.com>
2024-10-10 15:28:26 +0900

    쉼표 나오나

--- newfrontend/components/stock-chart.tsx
+++ newfrontend/components/stock-chart.tsx
@@ -121,7 +121,11 @@ const Stockchart = ({ stockGraphData }: StockChartProps) => {
     <div>
       <Line ref={chartRef} data={chartData} options={options} />
       <p className="text-center">
-        기준 가격: <strong className="font-bold">{firstPrice ? firstPrice.toLocaleString() : '가격 정보 없음'}</strong>원
+        기준 가격:{" "}
+        <strong className="font-bold">
+          {firstPrice ? firstPrice.toLocaleString("ko-KR") : "가격 정보 없음"}
+        </strong>
+        원
       </p>
     </div>
   );
