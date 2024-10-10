a2eb5448856031c4054a608c2c06632ccc0387cb
박상용 <psyok7@naver.com>
2024-10-10 18:58:15 +0900

    Fix: [FE] 전체 기능 수정

--- newfrontend/app/(auth)/page.tsx
+++ newfrontend/app/(auth)/page.tsx
@@ -31,7 +31,7 @@ const Login = (): JSX.Element => {
     const password = passwordRef.current?.value || "";
 
     try {
-      const response = await axios.post("http://j11a304.p.ssafy.io/api/users/login", {
+      const response = await axios.post("https://j11a304.p.ssafy.io/api/users/login", {
         email: id,
         password: password,
       }, {
